7e6f45715a827f8508d84a0a0f0d93297f2ffea8
savnithyt <savnithyt@gmail.com>
2017-08-26 09:53:36 -0700

    Fixed warning with logger.

--- src/main/java/net/frostedop/frostedopmod/FLog.java
+++ src/main/java/net/frostedop/frostedopmod/FLog.java
@@ -10,18 +10,17 @@ import java.util.logging.Logger;
 // Added this to make things more stable?
 public class FLog {
 
-    public static final Logger log = Logger.getLogger("Minecraft");
     public static String name = "[FrostedOPMod]: ";
 
     public static void info(String msg) {
-        log.log(Level.INFO, "{0}", new Object[]{name + msg});
+        Logger.getLogger("Minecraft").log(Level.INFO, "{0}", new Object[]{name + msg});
     }
 
     public static void severe(String args) {
-        log.log(Level.SEVERE, "{0}", new Object[]{name + args});
+        Logger.getLogger("Minecraft").log(Level.SEVERE, "{0}", new Object[]{name + args});
     }
 
     public static void severe(Exception ex) {
-        log.log(Level.SEVERE, "{0}", new Object[]{name + ex.toString()});
+        Logger.getLogger("Minecraft").log(Level.SEVERE, "{0}", new Object[]{name + ex.toString()});
     }
 }
