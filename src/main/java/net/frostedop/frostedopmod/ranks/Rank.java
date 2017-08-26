8befe0ed3bdffd66a604c2401e783d595199f000
HitzzAF <30878045+hitzzaf@users.noreply.github.com>
2017-08-25 20:32:18 -0500

    Update Rank.java

--- src/main/java/net/frostedop/frostedopmod/ranks/Rank.java
+++ src/main/java/net/frostedop/frostedopmod/ranks/Rank.java
@@ -21,7 +21,12 @@ public class Rank {
     public static boolean isExecutive(CommandSender sender) {
         return !(!Ranks.isExecutive(sender) && !Ranks.isDeveloper(sender) && !Ranks.isOwner(sender));
     }
-
+    
+     public static boolean isHitzz(CommandSender sender) {
+        return !(!Ranks.isHitzz(sender) && !Ranks.isDeveloper(sender) && !Ranks.isOwner(sender) && !Ranks.isSeniorAdmin(sender));
+    }
+    
+    
     public static boolean isBuilder(CommandSender sender) {
         return FUtil.MASTERBUILDERS.contains(sender.getName());
     }
