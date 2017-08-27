6ab7d8418a9e4666ac1528bd05dfe7e0f0a78ffa
savnithyt <savnithyt@gmail.com>
2017-08-26 21:22:05 -0700

    Fixed errors.

--- src/main/java/net/frostedop/frostedopmod/commands/C_warn.java
+++ src/main/java/net/frostedop/frostedopmod/commands/C_warn.java
@@ -1,9 +1,10 @@
 package net.frostedop.frostedopmod.commands;
 
 import static net.frostedop.frostedopmod.commands.FCommand.NO_PERM;
+import static net.frostedop.frostedopmod.commands.FCommand.PLAYER_NOT_FOUND;
 import net.frostedop.frostedopmod.ranks.Rank;
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,38 +12,51 @@ import org.bukkit.entity.Player;
 
 /**
  *
- * @author hailey
+ * @author hailey & savnith
  */
 public class C_warn extends FCommand {
-        
+
     public C_warn() {
         super("warn", "/warn <player> <reason>", "Warn a player for being bad.");
     }
 
     @Override
+    @SuppressWarnings("null")
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        
+
         if (!Rank.isAdmin(sender)) {
             sender.sendMessage(NO_PERM);
             return true;
         }
         
-        if (args.length == 0) {
+        if (args.length < 1) {
             return false;
         }
-        
-        Player player = (Player) sender;
-        
-        if (player == null) {
-            sender.sendMessage(ChatColor.RED + "Player not found!");
+
+        final Player player = Bukkit.getServer().getPlayer(args[0]);
+
+        String reason = null;
+
+        if (args.length > 1) {
+            reason = StringUtils.join(args, " ", 1, args.length);
         }
-        
-        final String warning = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
-        
-        player.sendMessage(ChatColor.LIGHT_PURPLE + "You have been warned by an admin for " + warning); 
-        
-        sender.sendMessage(ChatColor.RED + "You have warned " + player.getName() + " for: " + warning);
-        
+
+        if (player == null) {
+            sender.sendMessage(PLAYER_NOT_FOUND);
+            return true;
+        }
+
+        warn(player, sender, reason);
         return true;
-}
+    }
+
+    public static void warn(Player player, CommandSender sender, String reason) {
+        
+        if (reason != null) {
+            sender.sendMessage(ChatColor.RED + "You have successfully warned, " + player.getName() + " for (" + reason + ").");
+            player.sendMessage(ChatColor.RED + "You have been warned by " + sender.getName() + " for (" + reason + ").");
+        } else {
+            sender.sendMessage(ChatColor.GRAY + "Please provide a warn reason.");
+        }
+    }
 }
